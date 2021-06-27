5a1fbd59fd739610f28e2970f34267e954716ae6
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-06-27 22:01:12 +0300

    learn (insertAdjacentHTML)

--- js/article.js
+++ js/article.js
@@ -1,33 +1,58 @@
-import qweqwe from "./data/products.js";
-console.log(qweqwe);
+// import qweqwe from "./js/products.js";
+// console.log(qweqwe);
 
-const product = {
-  name: "Сервоприводы",
-  description: "Lorem ipsum — классический текст-«рыба».",
-  price: 2000,
-  available: true,
-  onSale: true,
+const products = [
+  {
+    name: "Сервоприводы",
+    description: "Lorem ipsum — классический текст-«рыба».",
+    price: 2000,
+    available: true,
+    onSale: true,
+  },
+  {
+    name: "Телевизоры",
+    description: "Lorem ipsum — классический текст-«рыба».",
+    price: 51000,
+    available: false,
+    onSale: true,
+  },
+
+  {
+    name: "Ноутбики",
+    description: "Lorem ipsum — классический текст-«рыба».",
+    price: 24500,
+    available: true,
+    onSale: true,
+  },
+];
+
+const makeProductCard = ({ name, description, price }) => {
+  const productEl = document.createElement("article");
+  productEl.classList.add("product");
+
+  const nameEl = document.createElement("h2");
+  nameEl.textContent = name;
+  nameEl.classList.add("product__name");
+
+  const descrEl = document.createElement("p");
+  descrEl.textContent = description;
+  descrEl.classList.add("product__descr");
+
+  const priceEl = document.createElement("p");
+  priceEl.textContent = `Цена: ${price} кредитов`;
+  priceEl.classList.add("product__price");
+
+  productEl.append(nameEl, descrEl, priceEl);
+
+  return productEl;
 };
 
-const productEl = document.createElement("article");
-productEl.classList.add("product");
+console.log(makeProductCard(products[0]));
 
-const nameEl = document.createElement("h2");
-nameEl.textContent = product.name;
-nameEl.classList.add("product__name");
+const elements = products.map(makeProductCard);
 
-const descrEl = document.createElement("p");
-descrEl.textContent = product.description;
-descrEl.classList.add("product__descr");
+console.log(elements);
 
-const priceEl = document.createElement("p");
-priceEl.textContent = `Цена: ${product.price} кредитов`;
-priceEl.classList.add("product__price");
+const productsConteinerEl = document.querySelector(".js-products");
 
-// console.log(productEl);
-// console.log(nameEl);
-// console.log(descrEl);
-// console.log(priceEl);
-
-productEl.append(nameEl, descrEl, priceEl);
-console.log(productEl);
+productsConteinerEl.append(...elements);
---
+++ js/pars.js
@@ -0,0 +1,6 @@
+const titleEl = document.querySelector(".title");
+
+// console.log(titleEl.textContent);
+// console.log(titleEl.innerHTML);
+
+titleEl.insertAdjacentHTML("beforeend", '<a href="">это ссылка</a>');
