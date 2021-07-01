19799ad862c40d343c5e1514ef828bc4458f97e8
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-01 22:41:55 +0300

    started doing hw

---
+++ homework-07/src/js/task-01.js
@@ -0,0 +1,7 @@
+const list = document.querySelector('#categories');
+
+console.log('Количество элементов:', list.children);
+const item = document.querySelectorAll('.item h2');
+console.log(item);
+
+// console.log(item);
