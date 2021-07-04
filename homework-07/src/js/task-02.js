ec1cd02b0efa3b943a7cdcd9dbaa1c01b2ec0bcf
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-04 12:52:39 +0300

    task 2 done

--- homework-07/src/js/task-02.js
+++ homework-07/src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const newItem = ingredients.map(item => {
+  const elemList = document.createElement('li');
+  elemList.textContent = item;
+  return elemList;
+});
+
+const listItem = document.querySelector('#ingredients');
+listItem.append(...newItem);
+console.log(listItem);
