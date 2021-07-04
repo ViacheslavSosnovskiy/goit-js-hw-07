84306052eb35e1a13ddc827fcf7cf73e8dc08773
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-04 16:39:01 +0300

    task 3, 4 done

---
+++ homework-07/src/js/task-04.js
@@ -0,0 +1,22 @@
+// const container = document.querySelector('#counter');
+const counterValue = document.querySelector('#value');
+let value = 0;
+const decrementBtn = document.querySelector(
+  '#counter button[data-action="decrement"]'
+);
+const incrementBtn = document.querySelector(
+  '#counter button[data-action="increment"]'
+);
+
+const increment = () => {
+  value += 1;
+  counterValue.textContent = value;
+};
+
+const decrement = () => {
+  value -= 1;
+  counterValue.textContent = value;
+};
+
+incrementBtn.addEventListener('click', increment);
+decrementBtn.addEventListener('click', decrement);
